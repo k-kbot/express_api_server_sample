54e55cde840b583ca40b86bbafef447243559c24
k-kbot <polykkpodh1243@gmail.com>
2021-05-08 18:36:48 +0900

    post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ text: "hello world" })
+  res.json({ text: "hello world" });
+});
+
+router.post('/', (req, res) => {
+  if (req.headers['content-type'] === "application/json") {
+    res.status(201).json(req.body);
+  } else {
+    res.status(400).send('Bad Request');
+  }
 });
 
 module.exports = router;
